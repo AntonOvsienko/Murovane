742e15b2a72403c77b40362f0dabddecd7e12b65
Anton_Ovsienko <Anton_Ovsienko@epam.com>
2022-12-06 18:52:21 +0200

    add logic for buy ticket

--- src/main/java/com/sale/ticket/task/controller/LogicController.java
+++ src/main/java/com/sale/ticket/task/controller/LogicController.java
@@ -5,6 +5,8 @@ import com.sale.ticket.task.service.BilletService;
 import com.sale.ticket.task.service.RouteService;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import net.bytebuddy.implementation.bind.annotation.Default;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,15 +40,17 @@ public class LogicController {
     }
 
     @PostMapping ("/buy-ticket")
-    public String addNewTicket(@RequestParam ("id") Integer id, @RequestParam ("firstname") String firstname, @RequestParam ("surname") String surname, @RequestParam ("patronomic") String patronomic, Model model) {
-        System.out.println(id);
-        Billet billet = new Billet();
-        billet.setFirstName(firstname);
-        billet.setSurname(surname);
-        billet.setPatronomic(patronomic);
-        Integer billetId = billetService.createNewTicket(billet, id);
-        model.addAttribute("billetIndex", billetId);
-        model.addAttribute("billetPresent", true);
+    public String addNewTicket(@RequestParam ("id")Integer id, @RequestParam ("firstname") String firstname, @RequestParam ("surname") String surname, @RequestParam ("patronomic") String patronomic, Model model) {
+        if (id != 0) {
+            System.out.println(id);
+            Billet billet = new Billet();
+            billet.setFirstName(firstname);
+            billet.setSurname(surname);
+            billet.setPatronomic(patronomic);
+            Integer billetId = billetService.createNewTicket(billet, id);
+            model.addAttribute("billetIndex", billetId);
+            model.addAttribute("billetPresent", true);
+        }
         List<Route> routeList = routeService.getAllRoute();
         model.addAttribute("routeList", routeList);
 
